1edb665cc2b842fac529e3c9de36da7601409022
“Antoine <antoine.louis02@outlook.fr>
2019-04-29 14:26:50 +0200

    Transform sound signal into binary code

--- channel_coding.py
+++ channel_coding.py
@@ -7,21 +7,33 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
 
+def encode(samples, n_bits):
+    """
+    Transform the sound signal using a (naive) fixed-length binary code.
+    """
+    binary_signal = ''
 
+    # Convert each sample into binary code and add it to signal
+    for sample in samples:
+        bin_sample = "{0:b}".format(sample)
+        prefix = '0' * (n_bits - len(binary_signal))
+        binary_signal += (prefix + bin_sample)
 
+    return binary_signal
 
 
 
 
 if __name__ == "__main__":
 
-    # Plot of the sound signal
-    sampling_rate, data = wavfile.read('sound.wav')
-    plt.plot(data)
+    # Q8: Plot of the sound signal
+    sampling_rate, signal_samples = wavfile.read('sound.wav')
+    plt.plot(signal_samples)
     plt.ylabel('Amplitude', fontsize=13)
     plt.xlabel('Time', fontsize=13)
     plt.title('Sample wav')
     plt.savefig('sound.eps')
 
-    
+    # Q9: Transform sound signal into binary code
+    binary_signal = encode(signal_samples, 8)
     
